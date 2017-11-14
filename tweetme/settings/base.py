ec90bcdf9ff077c21facea097442affea9b75e8e
Rommel Rico <me@rommelrico.com>
2017-11-14 08:11:12 -0800

    Django settings

--- tweetme/settings/base.py
+++ tweetme/settings/base.py
@@ -14,7 +14,7 @@ import os
 from configparser import RawConfigParser
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 config = RawConfigParser()
 config.read(os.path.join(BASE_DIR, 'config', 'settings.ini'))
