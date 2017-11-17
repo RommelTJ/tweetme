6da9186e9643061272740c748ccacca35a31a979
Rommel Rico <me@rommelrico.com>
2017-11-17 08:27:34 -0800

    Create view

--- tweets/forms.py
+++ tweets/forms.py
@@ -7,10 +7,8 @@ class TweetModelForm(forms.ModelForm):
     class Meta:
         model = Tweet
         fields = [
-            "user",
             "content"
         ]
-        #exclude = ['user']
 
     def clean_content(self, *args, **kwargs):
         content = self.cleaned_data.get("content")
