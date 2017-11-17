5eb88c8db9db31b389b9c196bf1ce2e9be67ebb3
Rommel Rico <me@rommelrico.com>
2017-11-17 08:18:40 -0800

    Model form

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
 from .models import Tweet
+from .forms import TweetModelForm
 
 
-admin.site.register(Tweet)
+class TweetModelAdmin(admin.ModelAdmin):
+    form = TweetModelForm
+
+admin.site.register(Tweet, TweetModelAdmin)
