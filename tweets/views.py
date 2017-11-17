582bbc777cba94f9e97ca439328686e7931faa41
Rommel Rico <me@rommelrico.com>
2017-11-16 18:30:27 -0800

    Dynamic URL Routing

--- tweets/views.py
+++ tweets/views.py
@@ -1,24 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView, ListView
 
 from .models import Tweet
 
 
 class TweetDetailView(DetailView):
-    #template_name = "tweets/detail_view.html"
     queryset = Tweet.objects.all()
-
-    def get_object(self):
-        return Tweet.objects.get(id=1)
-
 
 
 class TweetListView(ListView):
-    #template_name = "tweets/list_view.html"
     queryset = Tweet.objects.all()
 
     def get_context_data(self, *args, **kwargs):
         context = super(TweetListView, self).get_context_data(*args, **kwargs)
-        #context["another_list"] = Tweet.objects.all()
-        #print(context)
         return context
+
+
+def tweet_detail_view(request, pk=None): # pk == id
+    obj = get_object_or_404(Tweet, pk=pk)
+    print(obj)
+    context = {
+        "object": obj
+    }
+    return render(request, "tweets/detail_view.html", context)
