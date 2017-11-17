31b8055a750456ff764616547d8faac01cf83163
Rommel Rico <me@rommelrico.com>
2017-11-17 08:31:43 -0800

    User Needed & Login Required Mixins

--- tweets/views.py
+++ tweets/views.py
@@ -1,11 +1,13 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView, ListView, CreateView
 
 from .models import Tweet
 from .forms import TweetModelForm
+from .mixins import FormUserNeededMixin
 
 
-class TweetCreateView(CreateView):
+class TweetCreateView(FormUserNeededMixin, CreateView):
     form_class = TweetModelForm
     template_name = 'tweets/create_view.html'
     success_url = "/tweet/create/"
@@ -14,17 +16,6 @@ class TweetCreateView(CreateView):
         form.instance.user = self.request.user
         return super(TweetCreateView, self).form_valid(form)
 
-
-def tweet_create_view(request):
-    form = TweetModelForm(request.POST or None)
-    if form.is_valid():
-        instance = form.save(commit=False)
-        instance.user = request.user
-        instance.save()
-    context = {
-        "form":form
-    }
-    return render(request, 'tweets/create_view.html', context)
 
 class TweetDetailView(DetailView):
     queryset = Tweet.objects.all()
