247ef3d04bb2f86a944ede0ee1dee1bc026c7a97
Rommel Rico <rommeltj@gmail.com>
2017-11-23 19:06:59 -0800

    Toggle Follow

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,9 +3,11 @@ from django.conf.urls import url
 from django.views.generic.base import RedirectView
 
 from .views import (
-    UserDetailView
+    UserDetailView,
+    UserFollowView
 )
 
 urlpatterns = [
     url(r'^(?P<username>[\w.@+-]+)/$', UserDetailView.as_view(), name='detail'), # /tweet/1/
+    url(r'^(?P<username>[\w.@+-]+)/follow/$', UserFollowView.as_view(), name='follow'),
 ]
