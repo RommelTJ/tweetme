e587196f821b40ec81397fa3da36ba6b7de1a260
Rommel Rico <rommeltj@gmail.com>
2017-11-25 09:34:05 -0800

    Retweet Part 2

--- tweets/api/serializers.py
+++ tweets/api/serializers.py
@@ -5,11 +5,10 @@ from accounts.api.serializers import UserDisplaySerializer
 from tweets.models import Tweet # from ..models import Tweet
 
 
-class TweetModelSerializer(serializers.ModelSerializer):
-    user = UserDisplaySerializer(read_only=True)  #write_only
+class ParentTweetModelSerializer(serializers.ModelSerializer):
+    user = UserDisplaySerializer(read_only=True) #write_only
     date_display = serializers.SerializerMethodField()
     timesince = serializers.SerializerMethodField()
-    is_retweet = serializers.SerializerMethodField()
 
     class Meta:
         model = Tweet
@@ -20,13 +19,32 @@ class TweetModelSerializer(serializers.ModelSerializer):
             'timestamp',
             'date_display',
             'timesince',
-            'is_retweet',
         ]
 
-    def get_is_retweet(self, obj):
-        if obj.parent:
-            return True
-        return False
+    def get_date_display(self, obj):
+        return obj.timestamp.strftime("%b %d, %Y at %I:%M %p")
+
+    def get_timesince(self, obj):
+        return timesince(obj.timestamp) + " ago"
+
+
+class TweetModelSerializer(serializers.ModelSerializer):
+    user = UserDisplaySerializer(read_only=True) #write_only
+    date_display = serializers.SerializerMethodField()
+    timesince = serializers.SerializerMethodField()
+    parent = ParentTweetModelSerializer()
+
+    class Meta:
+        model = Tweet
+        fields = [
+            'id',
+            'user',
+            'content',
+            'timestamp',
+            'date_display',
+            'timesince',
+            'parent',
+        ]
 
     def get_date_display(self, obj):
         return obj.timestamp.strftime("%b %d, %Y at %I:%M %p")
