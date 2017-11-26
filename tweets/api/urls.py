02c7c37ccb71b6a988c789bb988278a8d043036d
Rommel Rico <rommeltj@gmail.com>
2017-11-26 13:49:05 -0800

    Like Tweet

--- tweets/api/urls.py
+++ tweets/api/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 
 from .views import (
+    LikeToggleAPIView,
     RetweetAPIView,
     TweetListAPIView,
     TweetCreateAPIView
@@ -9,5 +10,6 @@ from .views import (
 urlpatterns = [
     url(r'^$', TweetListAPIView.as_view(), name='list'),  # /api/tweet/
     url(r'^create/$', TweetCreateAPIView.as_view(), name='create'),  # /tweet/create/
+    url(r'^(?P<pk>\d+)/like/$', LikeToggleAPIView.as_view(), name='like-toggle'),
     url(r'^(?P<pk>\d+)/retweet/$', RetweetAPIView.as_view(), name='retweet') #/api/tweet/id/tweet/
 ]
